74ff07264a4e4e722b26184fddde249c075faab6
Ron Craig <ron.craig@comcast.net>
2018-09-26 03:22:22 -0700

    overhaul the repo, new conditions and specs, 7/9 specs are passsing

--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -2,22 +2,49 @@ require('rspec')
 require('pry')
 require('prime_sifting')
 
-
-
-describe('#number_sifter') do
-  # it("create a list of numbers from 2 to a given number") do
-  # number_input= Prime.new()
-  # expect(number_input.number_sifter(10)).to(eq([2, 3, 4, 5, 6, 7, 8, 9, 10]))
-  # end
-
-  # it("removes all multiples of 2 from a list of numbers") do
-  # number_input= Prime.new()
-  # expect(number_input.number_sifter(10)).to(eq([2, 3, 5, 7, 9]))
-  # end
-
-  it("removes all multiples of 2 and 3 from a list of numbers") do
-  number_input= Prime.new()
-  expect(number_input.number_sifter(10)).to(eq([2, 3, 5, 7]))
+describe('prime_sifter') do
+  it('creates an error message when a user inputs 1') do
+    number_input = Prime.new(1)
+    expect(number_input.prime_sifter).to(eq('zero is not a prime number'))
   end
 
+  it('creates an error message when a user inputs 2') do
+    number_input = Prime.new(2)
+    expect(number_input.prime_sifter).to(eq('one is not a prime number'))
+  end
+
+  it('creates an array of prime number when a user inputs 2') do
+    number_input = Prime.new(3)
+    expect(number_input.prime_sifter).to(eq([2]))
+  end
+
+  it('creates an array of prime number when a user inputs 4') do
+    number_input = Prime.new(4)
+    expect(number_input.prime_sifter).to(eq([2, 3]))
+  end
+
+  it('creates an array of prime number when a user inputs 4') do
+    number_input = Prime.new(8)
+    expect(number_input.prime_sifter).to(eq([2, 3, 5, 7]))
+  end
+
+  it('creates an array of prime number when a user inputs 4') do
+    number_input = Prime.new(9)
+    expect(number_input.prime_sifter).to(eq([2, 3, 5, 7]))
+  end
+
+  it('creates an array of prime number when a user inputs 4') do
+    number_input = Prime.new(9)
+    expect(number_input.prime_sifter).to(eq([2, 3, 5, 7]))
+  end
+
+  it('creates an array of prime number when a user inputs 10') do
+    number_input = Prime.new(10)
+    expect(number_input.prime_sifter).to(eq([2, 3, 5, 7]))
+  end
+
+  it('creates an array of prime numbers when a user inputs 18') do
+    number_input = Prime.new(18)
+    expect(number_input.prime_sifter).to(eq([2, 3, 5, 7, 11, 13, 17]))
+  end
 end
